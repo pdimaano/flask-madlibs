bccd7ec3dcda2beab616d7b6443095608eb47357
Gordon Ball <gball261@gmail.com>
2022-10-12 20:52:29 -0400

    Working madlibs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -10,10 +10,24 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/questions')
 def get_word_values():
-
-    words = silly_story.prompts
+    """"""
+    words = excited_story.prompts
 
     return render_template(
         "questions.html",
         words=words
+    )
+
+
+
+@app.get('/story')
+def show_story():
+    """"""
+    answers = request.args
+    # print("!!!!!!!!", answers["noun"])
+    text = excited_story.generate(answers)
+
+    return render_template(
+        "results.html",
+        text=text
     )
